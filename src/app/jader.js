a5bffe3aaeef366767d14beb275a004c84e12a1e
jaderfg <jaderflorez1998@gmail.com>
2022-02-28 22:41:11 -0500

    se agrega el proyecto

---
+++ src/app/jader.js
@@ -0,0 +1,14 @@
+await
+fetch
+const
+getLocationData
+const 
+import React, { useDebugValue } from 'react'
+
+export const jader = () => {
+  return (
+    <div>jader</div>
+  )
+}
+useDebugValue
+
